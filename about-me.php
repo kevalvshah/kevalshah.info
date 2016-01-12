37cd94bd4ec9cfb43a843ebd6e1ec4e5e552258b
Shah Keval <kevalvshah03@gmail.com>
2016-01-12 21:02:11 +0000

    slicing up the content for about-me.php

--- about-me.php
+++ about-me.php
@@ -1,5 +1,23 @@
 <?php include "tpl-files/header.tpl.php";
 ?>
-
+	<!-- <div class="jumbotron" >	 -->
+		<div class="container" id="about-me" >
+				<div>
+					<h1>Hello All,</h1>
+					<h2>A Little About me</h2>
+					<h2 style="font-size:5em;color:#e44d26;">I'm Keval Shah</h2>
+					<p>I am a front-end web developer with over 2 years of experience,</p>
+					<p>I am constantly improving my technical skills in my own time</p> 
+					<p>to go with hand to hand with technology,</p>
+					<p>I am fast learner and highly motivated team member and</p> 
+					<p>good experience in building websites with user-friendly</p>
+					<p>and rich experience.</p>
+					<p style="font-size:2em;">I am currently seeking a role as a <span style="color:#e44d26;">Front End Web Developer.</span></p>
+				</div>
+				<div>
+					<div></div>
+				</div>
+		</div>
+	</div>
 <?php include "tpl-files/footer.tpl.php";
 ?>
