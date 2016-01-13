0c4815b8f80c48e7843240a51684e841b804afb3
Shah Keval <kevalvshah03@gmail.com>
2016-01-13 20:52:03 +0000

    Completed Skills.php and deleting extra files

--- skills.php
+++ skills.php
@@ -1,5 +1,72 @@
 <?php include "tpl-files/header.tpl.php";
 ?>
 
+	<div class="tech-skills">
+		<div class="container-fluid">
+			<h1>Technical Skills</h1>
+
+			
+			<div id="img-only" style="margin-top:50px;">
+				<h3>&middot; &nbsp; <span>Web</span></h3>
+				<div class="skill-img">
+					<img  src="images/skills/logo-html5.svg" width="90px" height="90px"  >
+					<img  src="images/skills/css3.svg" width="90px" height="90px"  >
+					<img  src="images/skills/js.png" width="90px" height="90px"  >
+					<img  src="images/skills/logo_ajax.png" width="90px" height="90px"  >
+					<img  src="images/skills/php.svg" width="90px" height="90px"  >
+					<img  src="images/skills/jQuery-Logo.png" width="90px" height="90px"  >
+					<img  src="images/skills/sass.png" width="120px" height="90px"  >
+				</div>
+			</div>
+			<div class="clear"></div>
+			
+			<div id="img-only" >
+				<h3>&middot; &nbsp; <span>.NET</span></h3>
+				<div class="skill-img">
+					<img src="images/skills/asp.net.png" width="90px" height="90px" >
+					<img src="images/skills/vb.net.png" width="120px" height="90px" >
+				</div>
+			</div>
+			<div class="clear"></div>
+			
+			<div id="img-only">
+				<h3>&middot; &nbsp; <span>Database</span></h3>
+				<div class="skill-img">
+					<img src="images/skills/mysql-logo.svg" width="90px" height="90px" >
+					<img src="images/skills/sqlserver-logo.png" width="90px" height="90px" >
+				</div>
+			</div>
+			<div class="clear"></div>
+			
+			<div id="img-only">
+				<h3>&middot; &nbsp; <span>CMS</span></h3>
+				<div class="skill-img">
+					<img src="images/skills/metronic.png" width="120px" height="60px" >
+					<img src="images/skills/wordpress1.png" width="90px" height="90px" >
+				</div>
+			</div>
+			<div class="clear"></div>
+			
+			<div id="img-only">
+				<h3>&middot; &nbsp; <span>IDE TOOLS</span></h3>
+				<div class="skill-img">
+					<img src="images/skills/visual1.png" width="90px" height="90px" >
+					<img src="images/skills/dreamweaver.png" width="90px" height="90px" >
+					<img src="images/skills/Sublime.png" width="90px" height="90px" >
+					<img src="images/skills/Brackets.png" width="90px" height="90px" >
+					<img src="images/skills/photoshop.svg" width="90px" height="90px" >
+				</div>
+			</div>
+			<div class="clear"></div>
+			<div id="img-only">
+				<h3>&middot; &nbsp; <span>Repository</span></h3>
+				<div class="skill-img">
+					<img src="images/skills/Git.png" width="90px" height="80px" >
+				</div>
+			</div>
+		</div>
+	</div>
+
 <?php include "tpl-files/footer.tpl.php";
 ?>
+ 
