bff1deb3405a2f133cc27b455e0c4a886e3e3076
Shah Keval <kevalvshah03@gmail.com>
2016-04-07 15:27:06 +0100

    Adding up the portfolio and done with necessary changes

--- portfolio.php
+++ portfolio.php
@@ -11,11 +11,11 @@
 									<div class="carousel-inner" role="listbox">
 										<div class="item active"><img alt="First slide" class="first-slide" src="images/portfolio/yds-main.png"></div>
 										<div class="item"><img alt="Second slide" class="second-slide" src="images/portfolio/shriji1.png"></div>
-										<div class="item"><img alt="Third slide" class="third-slide" src="images/portfolio/unicodeg.png"></div>
+										<div class="item"><img alt="Third slide" class="third-slide" src="images/portfolio/home.png"></div>
 										<div class="item"><img alt="Fourth slide" class="fourth-slide" src="images/portfolio/yds-events.png"></div>
-										<div class="item"><img alt="Fifth slide" class="fivth-slide" src="images/portfolio/ath-products.png"></div>
-										<div class="item"><img alt="Sixth slide" class="fivth-slide" src="images/portfolio/unicodeg-s.png"></div>
-										<div class="item"><img alt="Seventh slide" class="fivth-slide" src="images/portfolio/train2secure.png"></div>
+										<div class="item"><img alt="Fifth slide" class="fifth-slide" src="images/portfolio/ai.png"></div>
+										<div class="item"><img alt="Sixth slide" class="sixth-slide" src="images/portfolio/unicodeg-s.png"></div>
+										<div class="item"><img alt="Seventh slide" class="seventh-slide" src="images/portfolio/train2secure.png"></div>
 									</div>
 								</div>
 							</div><!-- /.carousel -->
@@ -44,7 +44,7 @@
 			 			<a href="http://ananttextilehub.com/"><img src="images/portfolio/ath-logo.png" class="rounded" width="100%" height="360px"></a>
 				 	</div>
 				 	<div class="col-lg-4 col-sm-6" style="margin-top:30px;">
-			 			<a href="http://"><img src="images/background.png" class="rounded" width="100%" height="360px"></a>
+			 			<a href="http://aekaminc.com"><img src="images/portfolio/aifolio.png" class="rounded" width="100%" height="360px" ></a>
 				 	</div>
 				</div>
 			</div>
--- skills.php
+++ skills.php
@@ -7,61 +7,70 @@
 
 			
 			<div id="img-only" style="margin-top:50px;">
-				<h3>&diams; &nbsp; <span style="font-size:2.1em;">W</span>EB</h3>
+				<h3> &nbsp;&nbsp;&nbsp; <span style="font-size:2.1em;">W</span>EB</h3>
 				<div class="skill-img">
-					<img  src="images/skills/logo-html5.svg" width="90px" height="90px"  >
-					<img  src="images/skills/css3.svg" width="90px" height="90px"  >
-					<img  src="images/skills/js.png" width="90px" height="90px"  >
-					<img  src="images/skills/logo_ajax.png" width="90px" height="90px"  >
-					<img  src="images/skills/php.svg" width="90px" height="90px"  >
-					<img  src="images/skills/jQuery-Logo.png" width="90px" height="90px"  >
+					<img  src="images/skills/logo-html5.svg" width="75px" height="75px"  >
+					<img  src="images/skills/css3.svg" width="75px" height="75px"  >
+					<img  src="images/skills/js.png" width="75px" height="75px"  >
+					<img  src="images/skills/logo_ajax.png" width="75px" height="75px"  >
+					<img  src="images/skills/jQuery-Logo.png" width="75px" height="75px"  >
 					<img  src="images/skills/sass.png" width="120px" height="90px"  >
+				</div>
+			</div>
+			<div class="clear"></div>
+
+			<div id="img-only">
+				<h3> &nbsp;&nbsp;&nbsp; <span style="font-size:2.1em;">F</span>ramework</h3>
+				<div class="skill-img">
+					<img src="images/skills/laravel-logo.png" width="75px" height="75px" >
 				</div>
 			</div>
 			<div class="clear"></div>
 			
 			<div id="img-only" >
-				<h3>&diams; &nbsp; <span style="font-size:2.1em;">.N</span>ET</h3>
+				<h3>&nbsp;&nbsp;&nbsp; <span style="font-size:2.1em;">O</span>op</h3>
 				<div class="skill-img">
-					<img src="images/skills/asp.net.png" width="90px" height="90px" >
-					<img src="images/skills/vb.net.png" width="120px" height="90px" >
+					<img src="images/skills/php.svg" width="75px" height="75px" >
+					<img src="images/skills/asp.net.png" width="120px" height="75px" >
 				</div>
 			</div>
 			<div class="clear"></div>
 			
 			<div id="img-only">
-				<h3>&diams; &nbsp; <span style="font-size:2.1em;">D</span>ATABASE</h3>
+				<h3>&nbsp;&nbsp;&nbsp; <span style="font-size:2.1em;">D</span>ATABASE</h3>
 				<div class="skill-img">
-					<img src="images/skills/mysql-logo.svg" width="90px" height="90px" >
-					<img src="images/skills/sqlserver-logo.png" width="90px" height="90px" >
+					<img src="images/skills/mysql-logo.svg" width="75px" height="75px" >
+					<img src="images/skills/sqlserver-logo.png" width="75px" height="75px" >
+				</div>
+			</div>
+			<div class="clear"></div>
+
+			
+			
+			<div id="img-only">
+				<h3>&nbsp;&nbsp;&nbsp; <span style="font-size:2.1em;">C</span>MS</h3>
+				<div class="skill-img">
+					<img src="images/skills/metronic.png" width="120px" height="75px" >
+					<img src="images/skills/wordpress1.png" width="75px" height="75px" >
 				</div>
 			</div>
 			<div class="clear"></div>
 			
 			<div id="img-only">
-				<h3>&diams; &nbsp; <span style="font-size:2.1em;">C</span>MS</h3>
+				<h3>&nbsp;&nbsp;&nbsp; <span style="font-size:2.1em;">I</span>DE TOOLS</h3>
 				<div class="skill-img">
-					<img src="images/skills/metronic.png" width="120px" height="60px" >
-					<img src="images/skills/wordpress1.png" width="90px" height="90px" >
-				</div>
-			</div>
-			<div class="clear"></div>
-			
-			<div id="img-only">
-				<h3>&diams; &nbsp; <span style="font-size:2.1em;">I</span>DE TOOLS</h3>
-				<div class="skill-img">
-					<img src="images/skills/visual1.png" width="90px" height="90px" >
-					<img src="images/skills/dreamweaver.png" width="90px" height="90px" >
-					<img src="images/skills/Sublime.png" width="90px" height="90px" >
-					<img src="images/skills/Brackets.png" width="90px" height="90px" >
-					<img src="images/skills/photoshop.svg" width="90px" height="90px" >
+					<img src="images/skills/visual1.png" width="75px" height="75px" >
+					<img src="images/skills/dreamweaver.png" width="75px" height="75px" >
+					<img src="images/skills/Sublime.png" width="75px" height="75px" >
+					<img src="images/skills/Brackets.png" width="75px" height="75px" >
+					<img src="images/skills/photoshop.svg" width="75px" height="75px" >
 				</div>
 			</div>
 			<div class="clear"></div>
 			<div id="img-only">
-				<h3>&diams; &nbsp; <span style="font-size:2.1em;">R</span>EPOSITORY</h3>
+				<h3>&nbsp;&nbsp;&nbsp; <span style="font-size:2.1em;">V</span>ERSION CONTROL</h3>
 				<div class="skill-img">
-					<img src="images/skills/Git.png" width="100px" height="80px" >
+					<img src="images/skills/Git.png" width="75px" height="75px" >
 				</div>
 			</div>
 		</div>
